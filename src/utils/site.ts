bbedb8b9aa368694f7becf63e4583aacbdf1ea41
sesukneksa <sesukneksa@gmail.com>
2025-07-15 01:08:23 +0700

    Delete src directory

--- src/utils/site.ts
+++
@@ -1,4 +0,0 @@
-export const nama = 'Bokep Rumas';
-export const desk = 'Video Bokep Indo Jepang Jav Hijab Tante Viral Terbaru Bocil Ngentot Jilbab Smp Skandal';
-export const url = 'https://bokeprumas.pages.dev';
-export const terbit = '2025-06-27T00:01:03Z';
